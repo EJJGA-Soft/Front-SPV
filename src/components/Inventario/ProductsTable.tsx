66d753a66056e6e3fdcec4276ce2aabf5ac8804b
Anthony <anthowill1608@gmail.com>
2024-11-19 03:46:23 -0500

    Se realizo el Get de productos

--- src/components/Inventario/ProductsTable.tsx
+++ src/components/Inventario/ProductsTable.tsx
@@ -1,72 +1,73 @@
+import React from 'react';
 import { HiPencil, HiTrash } from 'react-icons/hi';
+import { ProductsTableProps } from '../../interfaces/Inventario/ProductsTableProps';
 
-const ProductsTable = () => {
+const ProductsTable: React.FC<ProductsTableProps> = ({ productos = [],
+  currentPage,
+  productosPerPage,
+  handleNextPage,
+  handlePrevPage, }) => {
+
+  // Calcular los índices para la paginación
+  const indexOfLastProducto = currentPage * productosPerPage;
+  const indexOfFirstProducto = indexOfLastProducto - productosPerPage;
+  const currentProductos = productos.slice(indexOfFirstProducto, indexOfLastProducto);
+
+
   return (
     <>
-        {/* TABLA */}
-        <div className="overflow-x-auto max-h-[calc(100vh-200px)]">
-          <table className="min-w-full bg-white shadow-md rounded-lg">
-            <thead>
-              <tr className="bg-white text-gray-700 text-center">
-                <th className="p-4 text-xs sm:text-base">Stock</th>
-                <th className="p-4 text-xs sm:text-base">Imagen</th>
-                <th className="p-4 text-xs sm:text-base">Nombre</th>
-                <th className="p-4 text-xs sm:text-base">Precio</th>
-                <th className="p-4 text-xs sm:text-base">Categoría</th>
-                <th className="p-4 text-xs sm:text-base">Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="border-t border-gray-200 text-center text-sm">
-                <td className="p-4 break-all">15</td>
-                <td className="py-3 px-6 text-center">
-                  <img
-                    alt="Imagen de Frijol 1kg"
-                    height={50}
-                    src="https://storage.googleapis.com/a1aa/image/ytf829Yj7NRlIqxhx8zdPeVzvA7238ffJaEOQ1ssNH9NrRDPB.jpg"
-                    width={50}
-                    className="mx-auto"
-                  />
-                </td>
-                <td className="p-4 break-all">Frijol</td>
-                <td className="p-4 break-all">$32.50</td>
-                <td className="p-4 break-all">Leguminosas</td>
-                <td className="p-4 flex justify-center space-x-4">
-                  <button
-                    className="text-blue-500 hover:text-blue-700"
-                    aria-label="Editar proveedor"
-                  >
-                    <HiPencil className="w-5 h-5" />
-                  </button>
+      {/* TABLA */}
+      <div className="overflow-x-auto max-h-[calc(100vh-200px)]">
+        <table className="min-w-full bg-white shadow-md rounded-lg">
+          <thead>
+            <tr className="bg-white text-gray-700 text-center">
+              <th className="p-4 text-xs sm:text-base">Stock</th>
+              <th className="p-4 text-xs sm:text-base">Imagen</th>
+              <th className="p-4 text-xs sm:text-base">Nombre</th>
+              <th className="p-4 text-xs sm:text-base">Precio</th>
+              <th className="p-4 text-xs sm:text-base">Categoría</th>
+              <th className="p-4 text-xs sm:text-base">Acciones</th>
+            </tr>
+          </thead>
 
-                  <button
-                    className="text-red-500 hover:text-red-700"
-                    aria-label="Eliminar proveedor"
-                  >
-                    <HiTrash className="w-5 h-5" />
-                  </button>
-                </td>
-              </tr>
+          <tbody>
+          {currentProductos.map((producto, index) => (
+            <tr key={index} className="border-t border-gray-200 text-center text-sm">
+              <td className="p-4 break-all">{producto.stock}</td>
+              <td className="py-3 px-6 text-center">
+                <img
+                  alt={producto.nombre}
+                  height={50}
+                  src={producto.urlImagen}
+                  width={50}
+                  className="mx-auto"
+                />
+              </td>
+              <td className="p-4 break-all">{producto.nombre}</td>
+              <td className="p-4 break-all">{producto.precio.toFixed(2)}</td>
+              <td className="p-4 break-all">{producto.categoriaId}</td>
+              <td className="p-4 flex justify-center space-x-4">
+                <button
+                  className="text-blue-500 hover:text-blue-700"
+                  aria-label="Editar proveedor"
+                >
+                  <HiPencil className="w-5 h-5" />
+                </button>
 
-            </tbody>
-          </table>
-        </div>
+                <button
+                  className="text-red-500 hover:text-red-700"
+                  aria-label="Eliminar proveedor"
+                >
+                  <HiTrash className="w-5 h-5" />
+                </button>
+              </td>
+            </tr>
+          ))}
 
-          {/* Paginacion */}
-          <div className="flex justify-between items-center mt-6">
-            <button
-              className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 disabled:opacity-50 text-xs sm:text-base"
-            >
-              Antes
-            </button>
-            <span className="text-gray-700 text-xs sm:text-base">Página</span>
-            <button
-              className="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 disabled:opacity-50 text-xs sm:text-base"
-            >
-              Siguiente
-            </button>
-          </div>
-        
+            
+          </tbody>
+        </table>
+      </div>
 
     </>
   );
--- src/modules/services/Inventario/InventoryService.tsx
+++ src/modules/services/Inventario/InventoryService.tsx
@@ -1,8 +1,23 @@
 import axios from "axios";
 import { Api_Connection } from "../API/api_connection";
+import { ResponseHelperModel } from "../../../interfaces/responseHelper_T_interface";
+import { Producto } from "../../../interfaces/Inventario/producto_interface";
 
 const url = `${Api_Connection()}Productos/`;
 
-export default class InventoryService {
-    
+export default class inventoryService {
+
+    // Obtener todos los productos
+    async getProducts(): Promise<ResponseHelperModel<Producto>> {
+        try {
+            const response = await axios.get(`${url}`);
+            return response.data as ResponseHelperModel<Producto>;
+        } catch (error) {
+            return {
+                success: false,
+                message: `Ha ocurrido un error al obtener los productos: ${error}`,
+            };
+        }
+    }
+
 }
