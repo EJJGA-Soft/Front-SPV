be7796bd4e96486d8c0f6de3ae6ddca648e3988a
Anthony <anthowill1608@gmail.com>
2024-11-19 01:18:25 -0500

    Se creo el servicio para el modulo de inventario

---
+++ src/modules/services/Inventario/InventoryService.tsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+import { Api_Connection } from "../API/api_connection";
+
+const url = `${Api_Connection()}Productos/`;
+
+export default class InventoryService {
+    
+}
