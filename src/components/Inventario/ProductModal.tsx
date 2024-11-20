fca9c6dc86e186e67007304f080f99909d7be924
Anthony <anthowill1608@gmail.com>
2024-11-20 11:33:04 -0500

    Modificación del modal de productos

--- src/components/Inventario/ProductModal.tsx
+++ src/components/Inventario/ProductModal.tsx
@@ -55,96 +55,134 @@ const ProductModal: React.FC<ProductModalProps> = ({ onClose }) => {
                         </button>
                     </div>
 
-                    <form className="p-3 space-y-3">
-                        {/* Sección de Dropzone */}
-                        <div
-                            {...getRootProps()}
-                            className={`border-2 border-dashed rounded p-4 text-center relative cursor-pointer h-40 ${
-                                isDragActive ? "bg-blue-100 border-blue-400" : "border-gray-300"
-                            }`}
-                        >
-                            <input {...getInputProps()} />
-                            {preview ? (
-                                <img
-                                    src={preview}
-                                    alt="Vista previa"
-                                    className="w-full h-full object-contain"
+                    <div className="max-h-[80vh] overflow-y-auto">
+                        <form className="p-3 space-y-3">
+                            {/* Sección de Dropzone */}
+                            <div
+                                {...getRootProps()}
+                                className={`border-2 border-dashed rounded p-4 text-center relative cursor-pointer h-40 ${isDragActive ? "bg-blue-100 border-blue-400" : "border-gray-300"
+                                    }`}
+                            >
+                                <input {...getInputProps()} />
+                                {preview ? (
+                                    <img
+                                        src={preview}
+                                        alt="Vista previa"
+                                        className="w-full h-full object-contain"
+                                    />
+                                ) : (
+                                    <div className="flex flex-col justify-center items-center h-full">
+                                        <div className="flex justify-center my-2">
+                                            <img
+                                                src="src/assets/icons/cloud-upload-svgrepo-com.png"
+                                                alt="Icono de subida"
+                                                className="w-10 h-10 sm:w-14 sm:h-14" // Imagen más pequeña en móvil
+                                            />
+                                        </div>
+                                        <p className="text-gray-600 font-semibold text-xs sm:text-sm">
+                                            {isDragActive
+                                                ? "Suelta los archivos aquí..."
+                                                : "Click para subir o arrastra y suelta"}
+                                        </p>
+                                        <p className="text-gray-600 font-semibold text-xs sm:text-sm">PNG, JPG</p>
+                                    </div>
+                                )}
+                            </div>
+
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Stock
+                                </label>
+                                <input
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
+                                    type="number"
+                                    required
                                 />
-                            ) : (
-                                <div className="flex flex-col justify-center items-center h-full">
-                                    <p className="text-gray-600 font-semibold text-xs sm:text-sm">
-                                        {isDragActive
-                                            ? "Suelta los archivos aquí..."
-                                            : "Click para subir o arrastra y suelta"}
-                                    </p>
-                                    <p className="text-gray-600 font-semibold text-xs sm:text-sm">PNG, JPG</p>
-                                </div>
-                            )}
-                        </div>
+                            </div>
 
-                        <div>
-                            <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
-                                Stock
-                            </label>
-                            <input
-                                className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
-                                type="number"
-                                required
-                            />
-                        </div>
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Nombre
+                                </label>
+                                <input
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
+                                    required
+                                />
+                            </div>
 
-                        <div>
-                            <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
-                                Nombre
-                            </label>
-                            <input
-                                className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
-                                required
-                            />
-                        </div>
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Precio
+                                </label>
+                                <input
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
+                                    type="number"
+                                    required
+                                />
+                            </div>
 
-                        <div>
-                            <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
-                                Precio
-                            </label>
-                            <input
-                                className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5"
-                                type="number"
-                                required
-                            />
-                        </div>
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Categoría
+                                </label>
+                                <select
+                                    name="rol"
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5 mb-6"
+                                >
+                                    <option value="">Selecciona una categoría</option>
+                                    <option value="admin">Bebidas</option>
+                                    <option value="user">Frutas y verduras</option>
+                                    <option value="user">Leguminosas</option>
+                                </select>
+                            </div>
 
-                        <div>
-                            <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
-                                Categoría
-                            </label>
-                            <select
-                                name="rol"
-                                className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5 mb-6"
-                            >
-                                <option value="">Selecciona una categoría</option>
-                                <option value="admin">Bebidas</option>
-                                <option value="user">Frutas y verduras</option>
-                                <option value="user">Leguminosas</option>
-                            </select>
-                        </div>
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Categoría
+                                </label>
+                                <select
+                                    name="rol"
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5 mb-6"
+                                >
+                                    <option value="">Selecciona una categoría</option>
+                                    <option value="admin">Bebidas</option>
+                                    <option value="user">Frutas y verduras</option>
+                                    <option value="user">Leguminosas</option>
+                                </select>
+                            </div>
 
-                        <div className="flex justify-center space-x-4 mt-6">
-                            <button
-                                type="button"
-                                onClick={onClose}
-                                className="text-gray-600 font-semibold bg-gray-200 hover:bg-gray-300 rounded-lg px-4 py-2 text-xs sm:text-sm"
-                            >
-                                Cancelar
-                            </button>
-                            <button
-                                type="submit"
-                                className="text-white font-semibold bg-blue-700 hover:bg-blue-800 rounded-lg px-4 py-2 text-xs sm:text-sm"
-                            >
-                                Guardar
-                            </button>
-                        </div>
-                    </form>
+                            <div>
+                                <label className="block mb-1 text-xs sm:text-sm font-medium text-gray-900">
+                                    Proveedor
+                                </label>
+                                <select
+                                    name="rol"
+                                    className="bg-gray-50 border border-gray-300 text-xs sm:text-sm rounded-lg block w-full p-2.5 mb-6"
+                                >
+                                    <option value="">Selecciona un proveedor</option>
+                                    <option value="admin">Bimbo</option>
+                                    <option value="user">Coca-Cola</option>
+                                    <option value="user">Marinela</option>
+                                </select>
+                            </div>
+
+                            <div className="flex justify-center space-x-4 mt-6">
+                                <button
+                                    type="button"
+                                    onClick={onClose}
+                                    className="text-gray-600 font-semibold bg-gray-200 hover:bg-gray-300 rounded-lg px-4 py-2 text-xs sm:text-sm"
+                                >
+                                    Cancelar
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="text-white font-semibold bg-blue-700 hover:bg-blue-800 rounded-lg px-4 py-2 text-xs sm:text-sm"
+                                >
+                                    Guardar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </>
